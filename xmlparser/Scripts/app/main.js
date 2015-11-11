a1d7f6e0fb230f465f7ea84072f1afd3de7ffb48
giles <giles.bradshaw@sigyl.com>
2015-11-11 23:31:23 +0000

    initial commit

---
+++ xmlparser/Scripts/app/main.js
@@ -0,0 +1,38 @@
+﻿requirejs.config({
+    baseUrl: 'scripts',
+    paths: {
+        jquery: 'jquery-2.1.4.min',
+        knockout: 'knockout-3.3.0',
+        punches: "knockout.punches.min",
+        xmlToJson: "xmlToJson",
+        linq: "linq.amd",
+
+        "ko-xml": "app/xml"
+
+    }
+});
+
+require(["jquery", "knockout", "punches", "ko-xml"], function ($, ko) {
+
+    function SomeComponentViewModel(params) {
+        // 'params' is an object whose key/value pairs are the parameters
+        // passed from the component binding or custom element.
+        this.params = params;
+    }
+ 
+    
+    
+
+    ko.components.register('xml', {
+        template: { require: 'text!components/xml.html' },
+        viewModel: SomeComponentViewModel
+    });
+
+
+
+    ko.punches.enableAll();
+    var viewModel = {
+        xml: ko.observable('<xml hello="oh" goodbye="ah">xml text  <morexml>some more text</morexml><evenmorexml>some more text</evenmorexml> and some text hee</xml>')
+    };
+    ko.applyBindings(viewModel);
+});
---
+++ xmlparser/Scripts/app/xml.js
@@ -0,0 +1,88 @@
+﻿define(["knockout", "xmlToJson", "linq"], function (ko, xmlToJson, linq) {
+    ko.virtualElements.allowedBindings.xmlToJson = true;
+    ko.bindingHandlers.xmlToJson = {
+        init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+            // Make a modified binding context, with a extra properties, and apply it to descendant elements
+            var error = ko.observable();
+            json = ko.pureComputed(function () {
+                try {
+                    error(undefined);
+                    return xmlToJson.parseString(ko.utils.unwrapObservable(valueAccessor()));
+                } catch (ex) {
+                    error(ex);
+                    return {};
+                }
+            });
+
+            var innerBindingContext = bindingContext.extend(
+                {
+                    $json: json,
+                    $jsonString: ko.pureComputed(function () { return JSON.stringify(json(), null, 4) }),
+                    $jsonError: error
+                }
+            );
+            ko.applyBindingsToDescendants(innerBindingContext, element);
+
+            // Also tell KO *not* to bind the descendants itself, otherwise they will be bound twice
+            return { controlsDescendantBindings: true };
+        }
+    };
+
+    
+
+
+    ko.virtualElements.allowedBindings.iterate = true;
+    ko.bindingHandlers.iterate = {
+        init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+            // Make a modified binding context, with a extra properties, and apply it to descendant elements
+            var innerBindingContext = bindingContext.extend(
+                {
+                    $xpath: ko.utils.unwrapObservable(valueAccessor().key) ?  (bindingContext.$xpath || '') + '/' + ko.utils.unwrapObservable(valueAccessor().key) : '',
+                    $iterate: ko.pureComputed(function () {
+                        var value = ko.utils.unwrapObservable(valueAccessor().data);
+                        return linq.From(value)
+                            .SelectMany(function (v) {
+                                return Object.keys(v)
+                                    .filter(
+                                        function (key) {
+                                            return key !== "_attr";
+                                        }
+                                    )
+                                    .map(
+                                        function (key) {
+                                            return {
+                                                key: key,
+                                                value: v[key],
+                                                
+                                            }
+                                        }
+                                    )
+                            }
+                        ).ToArray()
+                    }),
+                    $attributes: ko.pureComputed(function () {
+                        var value = ko.utils.unwrapObservable(valueAccessor().data);
+                        return linq.From(value)
+                            .Where(function (v) {
+                                return Object.keys(v).length && Object.keys(v)[0] === "_attr"
+                            })
+                            .SelectMany(function (attrs) {
+                                return Object.keys(attrs._attr).map(function (key) {
+                                    return {
+                                        key: key,
+                                        value: attrs._attr[key]._value
+                                    }
+                                })
+
+                            }).ToArray()
+                    })
+                }
+            );
+            ko.applyBindingsToDescendants(innerBindingContext, element);
+
+            // Also tell KO *not* to bind the descendants itself, otherwise they will be bound twice
+            return { controlsDescendantBindings: true };
+        }
+    };
+
+});
